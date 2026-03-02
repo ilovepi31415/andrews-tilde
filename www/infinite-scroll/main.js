3f53613b55bd1f800a00e1ea8ed98c55830156e7
David Randall <davidra@andrews.edu>
2026-03-02 02:10:28 -0500

    Add basic infinite scroller

--- www/infinite-scroll/main.js
+++ www/infinite-scroll/main.js
@@ -1,11 +1,24 @@
 const container = document.getElementById("container");
 
+const POIs = {
+    22: "Chandra Bahadur Dangi from Nepal was only 21.5 inches tall",
+    68: "This is how tall I am (on a good day)",
+    107: "The tallest person, Robert Wadlow, was this tall",
+    120: "You've reached 10 feet",
+    600: "This is 50 feet! Wow",
+    63360: "A full mile! I'm impressed"
+}
+
 let count = 0
 function AddChunk() {
     for (let i = 0; i < 10; i++) {
         const block = document.createElement("div");
         block.className = "inch";
         block.innerHTML = `${Math.floor(count / 12)}'${count % 12}"`;
+        if (Object.keys(POIs).includes(count.toString())) {
+            block.innerHTML += ` ${POIs[count]}`;
+            console.log("woah it worked")
+        }
         container.appendChild(block);
         count++;
     }
