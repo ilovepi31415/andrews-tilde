af57c7e0ff15e68f85695990ee771035bf9edf58
David Randall <davidra@andrews.edu>
2026-04-20 00:57:27 -0400

    Setup username changing

--- www/account/main.js
+++ www/account/main.js
@@ -1,4 +1,7 @@
-const btnLogout = document.getElementById("logout")
+const inputUsername = document.getElementById("change-username");
+const btnUsernameChange = document.getElementById("btn-username-change");
+const btnLogout = document.getElementById("logout");
+const currentUsername = document.getElementById("current-username");
 
 import { supabase } from "../supabase.js"
 
@@ -9,16 +12,36 @@ if (!user) {
 } else {
   console.log("not logged in")
 }
+updateUsername();
 
 btnLogout.addEventListener("click", async () => {
-    const { error }  = await supabase.auth.signOut();
+  const { error }  = await supabase.auth.signOut();
 
-    if (error) {
-        console.error("Logout failed:", error)
-    } else {
-        console.log("Logging Out")
-        window.location.href = "../index.html";
-} 
+  if (error) {
+      console.error("Logout failed: ", error)
+  } else {
+      console.log("Logging Out")
+      window.location.href = "../index.html";
+  } 
 });
 
-console.log("Hi")
+btnUsernameChange.addEventListener("click", async () => {
+  if (inputUsername) {
+    const { data, error } = await supabase.from("profiles").update({ username: inputUsername.value }).eq('id', user.id);
+    if (error) {
+      console.error("Update failed: ", error);
+    } else {
+      inputUsername.value = ""
+    }
+  }
+  updateUsername();
+});
+
+async function updateUsername() {
+  const { data, error } = await supabase.from("profiles").select("username").eq("id", user.id);
+  if (error) {
+    console.error("Update failed: ", error);
+  } else {
+    currentUsername.innerText = data[0].username;
+  }
+}
